b9121a79e88ba58ada381100a9ee45f65a5c5c55
muhammednazeer <nazeer4u2c@gmail.com>
2022-05-22 17:33:05 +0100

    Add DB URI and Todo model

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
+from distutils.command.config import config
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoappDB'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+   
+    
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+    
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data =[{
-        'description': 'Check my Mail'},
-        {'description': 'Repy Whatsapp message'},
-        {'description': 'Call my Mum'},
-    ])
+    return render_template('index.html', data = Todo.query.all())
+
+
 
 
 if __name__ == '__main__':
