6635dacbf88a06955f75a9ae8ab7d44e3559f280
muhammednazeer <nazeer4u2c@gmail.com>
2022-05-27 15:42:35 +0100

    implement database migration after adding a new column to the model

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoappDB'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-   
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
@@ -40,9 +41,23 @@ def create_todo():
     else:
         return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print(completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))    
+
 @app.route('/')
 def index():
-    return render_template('index.html', data = Todo.query.all())
+    return render_template('index.html', data = Todo.query.order_by('id').all())
 
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
+const checkboxes = document.querySelectorAll('.check-completed');
+for (let i = 0; i < checkboxes.length; i++) {
+    const checkbox = checkboxes[i];
+    checkbox.addEventListener('change', (e) => {
+        console.log('Event', e);
+        const newCompleted = e.target.checked;
+        const todoId = e.target.dataset.id;
+        fetch('/todos/' + todoId + '/set-completed', {
+            method: 'POST',
+            body: JSON.stringify({
+                'completed': newCompleted
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    });
+}
 const formSubmit = document.getElementById('form');
 formSubmit.addEventListener('submit', (e) => {
     e.preventDefault();
