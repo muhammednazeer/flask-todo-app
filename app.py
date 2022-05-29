f8753369ba2bf0004268b9e253f46818bf28ef96
muhammednazeer <nazeer4u2c@gmail.com>
2022-05-29 18:31:12 +0100

    add debuggin function to the models

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
-    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
+    todolist = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
     
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description}, list {self.list_id}>'
 
 #TODO LIST MODEL
 class TODOLIST(db.Model):
@@ -25,6 +25,9 @@ class TODOLIST(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'<TodoList {self.id} {self.name}>'
 
     
 db.create_all()
